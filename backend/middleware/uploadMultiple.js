8aa0010aa6f390f34adee99f04f12fd0c5974f15
magnifiq <124519076+magnifiq@users.noreply.github.com>
2024-06-19 16:27:35 +0300

    Update uploadMultiple.js

--- backend/middleware/uploadMultiple.js
+++ backend/middleware/uploadMultiple.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 const path = require("path");
 
 const storage = multer.diskStorage({
-  destination: "./tmp/uploads/",
+  destination: "/tmp/uploads/",
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}${path.extname(file.originalname)}`);
   },
