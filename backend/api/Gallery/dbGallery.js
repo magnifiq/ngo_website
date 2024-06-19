5ac8a6e43cbe5de8219a8195818d96b63da91709
magnifiq <124519076+magnifiq@users.noreply.github.com>
2024-06-19 16:52:02 +0300

    Update dbGallery.js

--- backend/api/Gallery/dbGallery.js
+++ backend/api/Gallery/dbGallery.js
@@ -3,14 +3,18 @@ require("dotenv").config();
 
 const galleryDbUri = process.env.GALLERY_CONN;
 
-const galleryConnection = mongoose.createConnection(galleryDbUri);
+function connectToGalleryDB() {
+  const galleryConnection = mongoose.createConnection(galleryDbUri);
 
-galleryConnection.on(
-  "error",
-  console.error.bind(console, "Gallery DB connection error:")
-);
-galleryConnection.once("open", function () {
-  console.log("Connected to Gallery DB");
-});
+  galleryConnection.on(
+    "error",
+    console.error.bind(console, "Gallery DB connection error:")
+  );
+  galleryConnection.once("open", function () {
+    console.log("Connected to Gallery DB");
+  });
 
-module.exports = galleryConnection;
+  return galleryConnection;
+}
+
+module.exports = connectToGalleryDB;
