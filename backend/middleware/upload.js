05840258bdee7f91cd7a314e1a90316600ac497b
magnifiq <124519076+magnifiq@users.noreply.github.com>
2024-06-19 16:21:03 +0300

    Update upload.js

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 const path = require('path');
 
 const storage = multer.diskStorage({
-    destination: './uploads/',
+    destination: './tmp/uploads/',
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}${path.extname(file.originalname)}`);
     },
