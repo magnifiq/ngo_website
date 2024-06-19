b0a6502264097ad3ea44e979e58722f4dc76be04
magnifiq <124519076+magnifiq@users.noreply.github.com>
2024-06-19 16:57:30 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,37 @@
-require('dotenv').config();
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const path = require('path');
-
-const newsRoutes = require('./api/News/newsRoutes');
+const newsRoutes = require("./api/News/newsRoutes");
+const galleryRoutes = require("./api/Gallery/galleryRoutes");
+const projectsRoutes = require("./api/ProjectsDescription/projectsRoutes");
+const eventsRoutes = require("./api/EventsDescription/eventsRoutes");
+const blogRoutes = require("./api/Blog/blogRoutes");
+const verifyToken = require("./middleware/authMiddleware");
+const userRoutes = require("./api/User/userRoutes");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-mongoose.connect(process.env.NEWS_CONN, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/news", newsRoutes);
+app.use("/projectsDescription", projectsRoutes);
+app.use("/eventsDescription", eventsRoutes);
+app.use("/gallery", galleryRoutes);
+app.use("/blog", blogRoutes);
+app.use("/auth", userRoutes);
+app.use("/admin", verifyToken, (req, res) => {
+  res.send("Admin content");
+});
 
-
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use('/news', newsRoutes);
+// Import the user connection to ensure the admin user is created
+require("./api/User/dbUser");
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
